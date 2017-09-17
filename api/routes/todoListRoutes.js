3993428e6aae9035be6fe178f0f78ff17d2efe8d
vmquan <vmquan310@gmail.com>
2017-09-17 12:05:14 +0700

    Quan commit 17/09/2017

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -3,18 +3,18 @@ module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
 
   // todoList Routes
-  app.route('/categories/getCategories')
+  app.route('/GET/categories')
     .get(todoList.list_all_categories);
 
-  app.route('/categories/getCategory/:categoryId')
+  app.route('/GET/categories/:categoryId')
     .get(todoList.read_a_category);
 
-  app.route('/categories/addCategory')
+  app.route('/POST/categories/:categoryId')
    .post(todoList.create_a_category);
 
-  app.route('/categories/updateCategory/:categoryId')
+  app.route('/PATCH/categories/:categoryId')
     .put(todoList.update_a_category);
    
-  app.route('/categories/deleteCategory/:categoryId')
+  app.route('/DELETE/categories/:categoryId')
     .delete(todoList.delete_a_category);
 };
