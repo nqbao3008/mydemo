d95313d2baf582881933496c73ab2906f387c58a
VinhNguyen0301 <marsino16050301zeroone@gmail.com>
2017-09-18 09:39:20 +0700

    Vinh commit

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -3,16 +3,16 @@ module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
 
   // todoList Routes
-  app.route('/GET/categories')
+  app.route('/GET/categories') 
     .get(todoList.list_all_categories);
 
   app.route('/GET/categories/:categoryId')
     .get(todoList.read_a_category);
 
-  app.route('/POST/categories/:categoryId')
+  app.route('/POST/categories')
    .post(todoList.create_a_category);
 
-  app.route('/PATCH/categories/:categoryId')
+  app.route('/PUT/categories/:categoryId')
     .put(todoList.update_a_category);
    
   app.route('/DELETE/categories/:categoryId')
